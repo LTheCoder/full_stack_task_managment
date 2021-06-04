cb2f4795bcd319274e8a550727bdc5809ed39998
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2021-06-04 14:22:23 +0300

    backend

--- App/src/app/app.component.ts
+++ App/src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs/operators'
+import { Lambda } from '../app/services/lambda'
+
+interface taskModel{
+  ID:string
+  task:string
+  value?:number
+}
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
+
 export class AppComponent {
+  tasksitem!:taskModel
   title = 'Task Manager';
+
+  constructor(private lambda:Lambda){
+    this.lambda.url('https://aj959vq0i9.execute-api.us-east-1.amazonaws.com/manage')
+  }
+  async getcall(){
+    this.tasksitem = await this.lambda.run("")
+    console.log(this.tasksitem)
+  }
+
 }
--- App/src/app/app.module.ts
+++ App/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +15,9 @@ import {MatCardModule} from '@angular/material/card';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
